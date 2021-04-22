417c2ede3670366f56ed15c29ad934a0381d52e4
Kevin Elliott <kevin@welikeinc.com>
2021-04-21 21:03:53 -0700

    Add gtag

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag-next";
 
-createApp(App).mount('#app')
+
+createApp(App)
+  .use(VueGtag, {
+    config: { id: "G-WBF050VEEQ" }
+  })
+  .mount('#app')
