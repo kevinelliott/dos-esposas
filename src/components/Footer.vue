512ae74edcc2bdaa5ec3eb7cd840c43a1e593d8e
Kevin Elliott <kevin@welikeinc.com>
2021-04-23 20:59:14 -0700

    Add disclaimer to footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,15 @@
 <template>
   <footer class="py-5 bg-black">
     <div class="container">
+      <p class="text-center text-muted mb-4 small">
+        Disclaimer:
+
+        Tezos and ꜩ are the rights of the Tezos Foundation and not us, and are referenced here to showcase the tools, libraries, &amp; integrations that Tezos offers.
+        Usage of this website and interactions with the blockchain are your responsibility, use with care!
+      </p>
+      <p class="text-center text-muted mb-4">
+        We <span class="text-danger">&#x2764;</span> Tezos!
+      </p>
       <p class="text-center text-white m-0 small">Copyright&nbsp;© Dos Esposas Restaurant Group 2021</p>
     </div>
   </footer>
